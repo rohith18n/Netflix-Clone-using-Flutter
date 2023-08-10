a092175d3b43d07b8d3276020e70b935cd4bdaea
Rohith N <rohith18n@gmail.com>
2023-08-10 16:07:37 +0530

    k image changed

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -6,16 +6,15 @@ const kHeight = SizedBox(height: 10);
 const kHeight20 = SizedBox(height: 20);
 const kHeight50 = SizedBox(height: 50);
 
-//Border radius values 
+//Border radius values
 final BorderRadius kRadius10 = BorderRadius.circular(10);
 final BorderRadius kRadius20 = BorderRadius.circular(20);
 final BorderRadius kRadius30 = BorderRadius.circular(30);
 
 //Image for showing if image not loaded
 const kMainImage =
-    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg";
+    "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bVpTTg0NvaS905U2oXVr0awgCbM.jpg";
 
 //TextStyle
 TextStyle kHomeTitleText =
     const TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
-
